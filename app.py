4f31b88973156addd8523e815e5d3f267c9798ec
shahzada3 <eliteprincesv@gmail.com>
2026-07-27 00:52:15 +0530

    use tensorflow compatible with python 3.14

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
 from PIL import Image
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # ---------------------------------------------------
 # Config
